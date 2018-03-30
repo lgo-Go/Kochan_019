aa4cd71e33f4868270b36473c93cc570f85215fc
Игорь <pupokbomzha@yandex.ru>
2018-03-30 23:52:22 +0300

    + 0.5 * 3

--- p7ex7.c
+++ p7ex7.c
@@ -1,6 +1,17 @@
+/*Поиск простых чисел до 150*/
+
 #include <stdio.h>
 
 int main(void)
 {
-	printf("%i\n", 5 % 2);
+	int P[150];
+	for(int i = 0, j = 2; i < 149; ++i, ++j)
+	{
+		P[i] = j;
+		if(j % 2 == 0 && j != 0)
+			j = 0;
+		printf("%3i\n", j);
+	}
+
+	return 0;
 }
